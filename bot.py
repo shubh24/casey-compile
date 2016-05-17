37069aabe46a1cb15bcd7c1633abef9cdeebebe4
shubh24 <shubhankar24@gmail.com>
2016-05-17 12:06:40 +0530

    Launch for Heroku

---
+++ bot.py
@@ -0,0 +1,30 @@
+from compile import *
+
+from flask import Flask, request, Response
+import json
+import requests
+
+
+import os
+
+# initialization
+app = Flask(__name__)
+app.config.update(
+    DEBUG = True,
+)
+
+@app.route("/compile", methods = ['GET', 'POST'])
+def interact():
+    if request.method == 'GET':
+        return "OKAY"
+    if request.method == 'POST':
+    	return "POST OKAY"
+
+@app.route('/')
+def hello():
+    return "hello"
+
+# launch
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
