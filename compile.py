f4b3a712b73298ccaac9b2c530c18b0d6d680060
shubh24 <shubhankar24@gmail.com>
2016-05-30 17:41:53 +0530

    video output via cv2

--- compile.py
+++ compile.py
@@ -6,13 +6,13 @@ from pytube import YouTube
 
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-frames_count = 24
+# frames_count = 24
 selected_frames = []
 
 def cluster(data, maxgap):
     groups = [[data[0]]]
     for x in data[1:]:
-        if abs(x - groups[-1][-1]) <= maxgap:
+        if abs(x - groups[-1][0]) <= maxgap:
             groups[-1].append(x)
         else:
             groups.append([x])
@@ -33,7 +33,7 @@ def detect_faces(img, counter):
 			width = f[2]
 			height = f[3]
 			face_percentage += (width*height)/(len(img)*len(img[0]))*100
-		if face_percentage > 15:
+		if face_percentage > 5: #CHANGE IT TO 15
 			selected_frames.append(counter)
 	# counter += 1
 	sys.stdout.write('\n')	
@@ -54,45 +54,52 @@ def compile(groups, clip):
 	return concat_clips
 
 
-def doIt(vlog_url):
-	y = YouTube(vlog_url)
-	if (y.filename + '.mp4') not in os.listdir("."):
-		print "downloading"
-		y.get('mp4','360p').download('.')
-		print "downloaded"
+#def doIt(vlog_url):
+def doIt(filename):
+	# y = YouTube(vlog_url)
+	# if (y.filename + '.mp4') not in os.listdir("."):
+	# 	print "downloading"
+	# 	y.get('mp4','360p').download('.')
+	# 	print "downloaded"
 
-	if y.filename + '_compile.mp4' not in os.listdir("."):
-		#clip = VideoFileClip(y.filename + '.mp4') 
-		clip = cv2.VideoCapture(y.filename + '.mp4')
-		#frames = clip.iter_frames()
-		print "yo"
-		counter = 0
+	# if y.filename + '_compile.mp4' not in os.listdir("."):
+		#clip = VideoFileClip(y.filename + '.mp4')
+	clip = cv2.VideoCapture(filename + '.mp4')
+	global frames_count, width, height
+	frames_count = int(clip.get(cv2.cv.CV_CAP_PROP_FPS))
+	width  = int(clip.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+	height = int(clip.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+	print frames_count
+	#frames = clip.iter_frames()
+	counter = 0
+	success,image = clip.read()
+	while(success):
 		success,image = clip.read()
-		while(success):
-			success,image = clip.read()
-			if (counter%frames_count == 0):
-				print counter
-				detect_faces(image, counter)
-			counter += 1
+		if (counter%frames_count == 0):
+			print counter
+			detect_faces(image, counter)
+		counter += 1
 
-		groups = cluster(selected_frames, maxgap = 10*frames_count)
+	groups = cluster(selected_frames, maxgap = 10*frames_count)
 
-		final_clip = compile(groups, clip)
-		print len(final_clip)
+	final_clip = compile(groups, clip)
+	print len(final_clip)
+	final_video = cv2.VideoWriter(filename + "_compile.avi", cv2.cv.CV_FOURCC('X','V','I','D'), frames_count,(width,height))
+	
 
-		final_video = cv2.VideoWriter("yolo.mp4", -1, frames_count,(640,360))
-
-		for v in final_clip:
-			final_video.write(v)
+	for v in final_clip:
+		final_video.write(v)
 		
-		print "written"
-
-	return y.filename + "_compile.mp4"
+	final_video.release()
+	cv2.destroyAllWindows()	
+	
+	# 	return y.filename + "_compile.mp4"
 
 if __name__ == '__main__':
 	
 	# input_file_name = sys.argv[1]
 	# output_file_name = sys.argv[2]
 	
-	doIt("https://www.youtube.com/watch?v=Kk2VTtNR3JA")
+	#doIt("https://www.youtube.com/watch?v=Kk2VTtNR3JA")
+	doIt("HOW TO CASEY NEISTAT A VLOG by Sara Dietschy")
 	
